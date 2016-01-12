5e2e99799105c3790385339e477abf02585be099
brownman <brownman556@gmail.com>
2016-01-12 18:23:24 +0200

    bower after npm

--- project/install.sh
+++ project/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /tmp/project
-npm link bower
-./node_modules/bower/bin/bower install -g -f --allow-root  --config.interactive=false 
-npm install -g
 
+npm install -g
+npm link
+./node_modules/bower/bin/bower install -g -f --allow-root  --config.interactive=false 
