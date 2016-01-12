96c619306ffa67134b9fb9b6bb2b27ffb5ebd797
brownman <brownman556@gmail.com>
2016-01-12 13:58:46 +0200

    Update install.sh

--- project/install.sh
+++ project/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /tmp/project
-npm install -g
 npm link bower
-( ./node_modules/bower/bin/bower install -g -f --allow-root  --config.interactive=false ) || true 
+./node_modules/bower/bin/bower install -g -f --allow-root  --config.interactive=false 
+npm install -g
